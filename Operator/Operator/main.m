ec52c3846de9e76dbebf9277a4101c3ec583feaa
Small Rui <603049583@qq.com>
2018-07-12 00:56:13 +0800

    Operator

---
+++ Operator/Operator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Operator
+//
+//  Created by Small Rui on 2018/7/12.
+//  Copyright © 2018年 Trust. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
